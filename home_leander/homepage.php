de48918c4c2fc3237df80506b7181b967844e5eb
mickbosman <42744703+mickbosman@users.noreply.github.com>
2020-01-29 22:29:55 +0100

    fakka

---
+++ home_leander/homepage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="style/style.css">
+    <meta charset="utf-8">
+    <title>homepage one</title>
+  </head>
+  <body>
+    <div class="header">
+      <nav id="page-nav">
+      <!-- [THE HAMBURGER] -->
+      <label for="hamburger">&#9776;</label>
+      <input type="checkbox" id="hamburger"/>
+      <h1 class="naam__bedrijf">Snelle Chaps </h2>
+
+      <!-- [MENU ITEMS] -->
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Bestelling</a></li>
+        <li><a href="">Favorieten</a></li>
+        <li><a href="">Taal</a></li>
+        <li><a href="">Uiloggen</a></li>
+      </ul>
+    </nav>
+    </div>
+    <div>
+    <img src="img/background_homepage.jpg" alt="homepage image" class="homepage__image">
+    <div class="centered">Beste eten speciaal voor jou</div>
+    </div>
+
+    <div class="nav__eten">
+      
+        <p>Pizza</p>
+        <p>Döner</p>
+        <p>Burgers</p>
+
+    </div>
+
+  </body>
+</html>
