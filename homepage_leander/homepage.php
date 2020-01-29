24f28a996bb31b727ab657198ebbdeb52e4195cf
noxion1 <jolopolo3@gmail.com>
2020-01-29 22:36:23 +0100

    walouw
    kanker

---
+++ homepage_leander/homepage.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="style/style.css">
+    <meta charset="utf-8">
+    <title>homepage one</title>
+  </head>
+  <body>
+    <div class="header">
+      <nav id="page-nav">
+      <!-- [THE HAMBURGER] -->
+      <label for="hamburger">&#9776;</label>
+      <input type="checkbox" id="hamburger"/>
+      <h1 class="naam__bedrijf">Snelle Chaps </h2>
+
+      <!-- [MENU ITEMS] -->
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Bestelling</a></li>
+        <li><a href="">Favorieten</a></li>
+        <li><a href="">Taal</a></li>
+        <li><a href="">Uiloggen</a></li>
+      </ul>
+    </nav>
+    </div>
+    <div>
+    <img src="img/background_homepage.jpg" alt="homepage image" class="homepage__image">
+    <div class="centered">Beste eten speciaal voor jou</div>
+    </div>
+
+    <div class="nav__eten">
+        <a href=""><p class="helpmij4"><strong><</strong></p></a>
+        <a href=""><p class="helpmij">Pizza</p></a>
+        <a href=""><p class="helpmij2">Döner</p></a>
+        <a href=""><p class="helpmij3">Burgers</p></a>
+        <a href=""><p class="helpmij4"><strong>></strong></p></a>
+    </div>
+    <input type="text" placeholder="Zoek op Restaurant naam" class="searchbar"><img src="https://media.discordapp.net/attachments/669825056834191371/669825214301077514/icon.png?width=335&height=367" class="zoeken"></input>
+
+    <div class="pisiruimte">
+
+    </div>
+
+    <div class="restaurantLijst">
+      <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825213583720467/dominos.png" alt="" class="restaurantLijst__logo">
+      <h1>Dominos Pizza</h1>
+      <p class="description">Amerikaanse pizza, Snacks</p>
+      <p class="informatie">
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825278280990730/time.png" alt="" class="tijd">
+        Vanaf 15:00
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825163629690880/car1.png" alt="" class="bezorging">
+        €1,00
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825326603567124/shopping-bag-icon-png-29.jpg" alt="" class="minimun">
+        Min.  €9,50
+      </p>
+    </div>
+
+    <div class="restaurantLijst">
+      <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825217668841473/New.png" alt="" class="restaurantLijst__logo">
+      <h1>NewYork Pizza</h1>
+      <p class="description">Amerikaanse pizza, Italiaanse pizza </p>
+      <p class="informatie">
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825278280990730/time.png" alt="" class="tijd">
+        Vanaf 14:00
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825163629690880/car1.png" alt="" class="bezorging">
+        Gratis
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825326603567124/shopping-bag-icon-png-29.jpg" alt="" class="minimun">
+        Min.  €10,50
+      </p>
+    </div>
+
+    <div class="restaurantLijst">
+      <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825161888923668/1024px-KFC_logo_svg.png" alt="" class="restaurantLijst__logo">
+      <h1>Kentucky Fried Chicken</h1>
+      <p class="description">Chicken, Fries </p>
+      <p class="informatie">
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825278280990730/time.png" alt="" class="tijd">
+        Vanaf 12:00
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825163629690880/car1.png" alt="" class="bezorging">
+        Gratis
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825326603567124/shopping-bag-icon-png-29.jpg" alt="" class="minimun">
+        Min:  €8,00
+      </p>
+    </div>
+
+    <div class="restaurantLijst">
+      <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825163033968660/4581px-Papa_Johns_Logo_2019.svg.png" alt="" class="restaurantLijst__logo">
+      <h1>Papa John's</h1>
+      <p class="description">Italiaanse pizza, Amerikaanse pizza</p>
+      <p class="informatie">
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825278280990730/time.png" alt="" class="tijd">
+        Vanaf 00:00
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825163629690880/car1.png" alt="" class="bezorging">
+        €€3,00
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825326603567124/shopping-bag-icon-png-29.jpg" alt="" class="minimun">
+        Min.  €7,00
+      </p>
+    </div>
+
+    <div class="restaurantLijst">
+      <img src="https://media.discordapp.net/attachments/669825056834191371/669825170596429834/logo_465x320_1.png" alt="" class="restaurantLijst__logo">
+      <h1>Steakhouse Osdorp</h1>
+      <p class="description">Snacks, Italiaanse pizza, Steaks</p>
+      <p class="informatie">
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825278280990730/time.png" alt="" class="tijd">
+        Vanaf 14:00
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825163629690880/car1.png" alt="" class="bezorging">
+        Gratis
+        <!-- -->
+        <img src="https://cdn.discordapp.com/attachments/669825056834191371/669825326603567124/shopping-bag-icon-png-29.jpg" alt="" class="minimun">
+        Min.  €6,00
+      </p>
+    </div>
+
+
+  </body>
+</html>
